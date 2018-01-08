7f9003833a6f04310ee240b7dd96e85139c8aeee
csbrasnett <35073246+csbrasnett@users.noreply.github.com>
2018-01-08 15:13:28 +0000

    Added La and HII phase ID
    - added a module to attempt to identify La and HII phases - but this should be accessed separately, distinguished by the number of peaks found by finder.py
    
    -  added more comments about how things work at the top.

--- phase_ID.py
+++ phase_ID.py
@@ -4,14 +4,89 @@
 
 This programme will take an input array of peaks in 1D I vs q data (such as those returned from the finder programme),
 and returns a dictionary of possible phases that the data can take on, along with the miller plane index and the peaks 
-used to for that possible phase assignment. Also needs to be passed is a threshold for the number of binned values in 
-the histogram. A value of around 8 should suffice for this purpose.
+used to for that possible phase assignment. There are separate (but almost identical) methods for distinguishing cubic phases and 
+Lamellar/Inverse Hexagonal ones. It is recommended that having used the peak finding programme, the phase is attempted to be assigned
+by using the number of peaks found in the data. In general from the author's experience, the La and HII phases produce fewer Bragg peaks,
+such that if a condition were used along the lines of if len(peaks)<3: La_HII_possible_phases(peaks, etc) else: Q_possible_phases(peaks etc)
+then there should be a good chance of assigning the correct phase. Otherwise there is a risk of simultaneously assigning the HII along 
+with a cubic one. Worst comes to worst... The old fashioned hand method won't fail... a quick example set of imaginary data are at the
+bottom of the code to see how it functions/what results to expect.
+
+The information passed to the dictionary at the end should be enough to plot I vs q data with information about which peak has been
+indexed as which, along with information about the lattice parameter and phase. See the optional plot in the finder.py programme for
+more of an idea about the kind of way that matplotlib can plot something like this, using a combination of plt.axvline and plt.text.
 """
 
 
+import numpy as np
+"""
+La_HII_possible_phases works similarly to Q_possible_phases, in that it uses a statistical methodology to work out which peaks can 
+be assigned to which phase. However, as fewer peaks are expected to be passed to this module, it simply determines the phase by finding
+a consistent lattice parameter, and taking the longest assignment from La or HII given to it.
+
+La_HII_possible_phases will return a dictionary keyed by phase name, with values of lattice parameter, hkl plane factors, and the peaks
+correspondingly assigned.
+
+pass the following parameters to this function:
+    peaks - an array of peaks that have previously been found elsewhere
+    
+    bin_factor - a value to choose the number of bins. The number of bins in the histogram is calculated as the product of the 
+                 bin_factor value and the length of the flattened matrix of possible lattice parameter values. Take care such
+                 that this value must make the bins values an integer.
+"""
+def La_HII_possible_phases(peaks,bin_factor):
+    La_ratios=np.array([1,2,3])[:,np.newaxis]
+    HII_ratios=np.sqrt(np.array([1,3,4])[:,np.newaxis])
+    
+    La_init = 2*np.pi*(1/peaks)*La_ratios
+    HII_init = (2/np.sqrt(3))*2*np.pi*(1/peaks)*HII_ratios
+
+    La=np.ndarray.flatten(La_init)
+    HII=np.ndarray.flatten(HII_init)
+    
+    values=np.concatenate((La,HII))
+    
+    hist,bin_edges=np.histogram(values,bins=bin_factor*np.size(values))
+    
+    inds=np.digitize(values,bin_edges)-1 
+    
+    hist_max_bin_pos=np.where(inds==np.argmax(hist))[0]
+    
+    La_sourced=hist_max_bin_pos[np.where(hist_max_bin_pos<len(La))]   
+    HII_sourced=hist_max_bin_pos[np.where(hist_max_bin_pos>len(La)-1)]
+    
+    n=np.reshape(np.arange(0,np.size(La_init)),np.shape(La_init))
+    
+    La_peaks=np.zeros(0)
+    La_factors=np.zeros(0)
+    HII_peaks=np.zeros(0)
+    HII_factors=np.zeros(0)
+    
+    for a in range(0,len(La_sourced)):        
+        La_hkl=La_ratios[np.where(np.mod(La_sourced[a],np.size(n))==n)[0]][0][0]
+        La_peak=peaks[np.where(np.mod(La_sourced[a],np.size(n))==n)[1]][0]
+        
+        La_peaks=np.append(La_peaks,La_peak)
+        La_factors=np.append(La_factors,La_hkl)
+        
+    for b in range(0,len(HII_sourced)):        
+        HII_hkl=HII_ratios[np.where(np.mod(HII_sourced[b],np.size(n))==n)[0]][0][0]
+        HII_peak=peaks[np.where(np.mod(HII_sourced[b],np.size(n))==n)[1]][0]
+        
+        HII_peaks=np.append(HII_peaks,HII_peak)
+        HII_factors=np.append(HII_factors,HII_hkl)
+
+    phase_dict={}
+    if len(La_peaks)>len(HII_peaks):
+        phase_dict['La']=np.mean(values[np.where(inds==np.argmax(hist))]),La_factors,La_peaks
+    
+    elif len(HII_peaks)>len(La_peaks):
+        phase_dict['HII']=np.mean(values[np.where(inds==np.argmax(hist))]),HII_factors,HII_peaks
+        
+    return phase_dict
 
 """
-possible_phases works by creating matrices of lattice parameter values that can arise having declared that any peak that 
+Q_possible_phases works by creating matrices of lattice parameter values that can arise having declared that any peak that 
 has been found can be indexed as any miller index for any phase. These values are then collapsed into a single 1D array,
 which is investigated as a histogram. The number of bins in teh histogram is arbitrarily taken as twice the number of values,
 so care should taken. Peaks in the histogram will arise at the points where there are matching values 
@@ -21,7 +96,7 @@ that because of symmetry degeneracies, 'correct' phase values may arise from mor
 in the bins which exceed threshold population are then investigated for their origins: which peak and index were 
 responsible for bringing them about? 
 
-The possible_phases will return a dictionary, keyed through lattice parameters, with associated values of the phase (D=0, P=1, G=3),
+The Q_possible_phases will return a dictionary, keyed through lattice parameters, with associated values of the phase (D=0, P=1, G=3),
 the peaks that have been indexed, and the indicies assigned to the peak.
 
 pass the following parameters to this function:
@@ -37,9 +112,7 @@ pass the following parameters to this function:
 
 """
 
-import numpy as np
-
-def possible_phases(peaks,bin_factor, threshold):
+def Q_possible_phases(peaks,bin_factor, threshold):
         
     #define the characteristic peak ratios
     QIID=np.array([2,3,4,6,8,9,10,12])[:,np.newaxis]
@@ -93,15 +166,14 @@ def possible_phases(peaks,bin_factor, threshold):
                 D_factors=np.where(final_pos_array<len(D))[0][0:]
                 P_factors=(np.where(final_pos_array<=(len(P)+len(D))-1)[0][0:])[len(D_factors):]
                 G_factors=np.where(final_pos_array>(len(P)+len(D))-1)[0][0:]
-    
+     
                 #correspond the positions in the factors arrays to where they come from in the final positions array            
                 D_sourced=final_pos_array[D_factors].astype(int)
                 P_sourced=final_pos_array[P_factors].astype(int)
                 G_sourced=final_pos_array[G_factors].astype(int)
                 
                 #want to find where the matching phases have come from in the array to see which one is the real one.
-                #
-                #np.mod(o_sourced[a],n) corrects the position in the o array for running the same length as the sourced array
+                #e.g. np.mod(o_sourced[a],n) corrects the position in the o array for running the same length as the sourced array
                 #then find where the value is the same to identify the row
                 #then find from which ratio factor the peak originated from.         
                 D_sourced_factors=np.zeros(0,dtype=np.int)
@@ -130,7 +202,7 @@ def possible_phases(peaks,bin_factor, threshold):
                     P_sourced_factors=np.append(P_sourced_factors,P_hkl)
                     P_sourced_peaks=np.append(P_sourced_peaks,P_peak_hkl)
                 if len(P_sourced_factors) >3:
-                    phase_dict[values[P_sourced[a]]] = 1, P_sourced_factors, P_sourced_peaks
+                    phase_dict[values[P_sourced[b]]] = 1, P_sourced_factors, P_sourced_peaks
                     
                 for c in range(0,len(G_sourced)):
                     G_hkl=QIIG[np.where(np.mod(G_sourced[c],np.size(n))==n)[0]][0][0]
@@ -139,7 +211,7 @@ def possible_phases(peaks,bin_factor, threshold):
                     G_sourced_factors=np.append(G_sourced_factors,G_hkl)
                     G_sourced_peaks=np.append(G_sourced_peaks,G_peak_hkl)
                 if len(G_sourced_factors) >3:
-                    phase_dict[values[G_sourced[a]]] = 2, G_sourced_factors, G_sourced_peaks
+                    phase_dict[values[G_sourced[c]]] = 2, G_sourced_factors, G_sourced_peaks
                
         except IndexError:
             pass
@@ -212,8 +284,8 @@ def main(peaks,bin_factor,threshold,lo_q):
     QIID_ratios=np.array([2,3,4,6,8,9])
     QIIP_ratios=np.array([2,4,6,8,10,12])
     QIIG_ratios=np.array([6,8,14,16,20,22])
-
-    phases=possible_phases(peaks,1,threshold)
+        
+    phases=Q_possible_phases(peaks,1,threshold)
     clar={}
     for value in phases.items():
         fundamental=np.mean(value[1][2]/np.sqrt(value[1][1]))
@@ -230,3 +302,11 @@ def main(peaks,bin_factor,threshold,lo_q):
             if test3==1:
                 clar['G']=value[0],value[1][1],value[1][2]
     return clar
+
+#QIIP=np.sqrt(np.array([2,4,6,8,10]))
+#fundamental=0.06
+#Q_peaks=np.random.normal((QIIP*fundamental),0.0001)
+#Q_test=main(Q_peaks,2,5,0.06)
+#print('some example QIIP peaks', Q_peaks)
+#print('results of Q test', Q_test)
+
